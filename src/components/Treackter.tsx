9efa3ea67916d3d8ea936bb92396b3cfeeabee02
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-20 21:27:29 -0600

    Finish about button

--- src/components/Treackter.tsx
+++ src/components/Treackter.tsx
@@ -1,3 +1,5 @@
+import About from './About';
+
 function Treackter() {
   return (
     <div className="flex h-screen items-center">
@@ -10,9 +12,7 @@ function Treackter() {
       </div>
       {/* About Section */}
       <div className="w-16">
-        <svg className=" ml-auto fill-white w-[18px]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-          <path d="M41.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.3 256 246.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/>
-        </svg>
+        <About />
       </div>
     </div>
   );
