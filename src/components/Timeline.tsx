54d90597b7dad5c89d849b940d598ef938ade25e
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-26 14:26:56 -0600

    Align timeline correctly

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -210,32 +210,32 @@ function Timeline() {
         </div>
         <div className="w-[90%] flex items-center gap-10 text-sm text-center">
           {/* First Bar */}
-          <div className="w-[20%] flex justify-center gap-6">
-            <span className="w-[25%]">1</span>
-            <span className="w-[25%]">1.2</span>
-            <span className="w-[25%]">1.3</span>
-            <span className="w-[25%]">1.4</span>
+          <div className="w-[20%] flex justify-center gap-4">
+            <span className="w-[18%]">1</span>
+            <span className="w-[18%]">1.2</span>
+            <span className="w-[18%]">1.3</span>
+            <span className="w-[18%]">1.4</span>
           </div>
           {/* Second Bar */}
-          <div className="w-[20%] flex justify-center gap-6">
-            <span className="w-[25%]">2</span>
-            <span className="w-[25%]">2.2</span>
-            <span className="w-[25%]">2.3</span>
-            <span className="w-[25%]">2.4</span>
+          <div className="w-[20%] flex justify-center gap-4">
+            <span className="w-[18%]">2</span>
+            <span className="w-[18%]">2.2</span>
+            <span className="w-[18%]">2.3</span>
+            <span className="w-[18%]">2.4</span>
           </div>
           {/* Third Bar */}
-          <div className="w-[20%] flex justify-center gap-6">
-            <span className="w-[25%]">3</span>
-            <span className="w-[25%]">3.2</span>
-            <span className="w-[25%]">3.3</span>
-            <span className="w-[25%]">3.4</span>
+          <div className="w-[20%] flex justify-center gap-4">
+            <span className="w-[18%]">3</span>
+            <span className="w-[18%]">3.2</span>
+            <span className="w-[18%]">3.3</span>
+            <span className="w-[18%]">3.4</span>
           </div>
           {/* Fourth Bar */}
-          <div className="w-[20%] flex justify-center gap-6">
-            <span className="w-[25%]">4</span>
-            <span className="w-[25%]">4.2</span>
-            <span className="w-[25%]">4.3</span>
-            <span className="w-[25%]">4.4</span>
+          <div className="w-[20%] flex justify-center gap-4">
+            <span className="w-[18%]">4</span>
+            <span className="w-[18%]">4.2</span>
+            <span className="w-[18%]">4.3</span>
+            <span className="w-[18%]">4.4</span>
           </div>
           {/* Audio Manipulation */}
           <div className="w-[20%] flex gap-5 justify-end">
