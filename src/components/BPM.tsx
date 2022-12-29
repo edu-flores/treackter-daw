1da58618c81827606bf6e7999cb13d68d958fe08
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-28 23:12:08 -0600

    Change BPM input to range

--- src/components/BPM.tsx
+++ src/components/BPM.tsx
@@ -6,26 +6,35 @@ type Props = {
 const BPM = ({ bpm, setBpm }: Props) => {
 
   // Modify BPM on the DAW
-  const changeBPM = (val: number) => {
+  const changeBPM = (val: number, modifier=0) => {
+    // Arrow keys on screen
+    val += modifier;
+
+    // Valid range
     val = (val < 60) ? 60 : val;
-    val = (val > 120) ? 120 : val;
+    val = (val > 240) ? 240 : val;
     setBpm(val);
   }
 
   return (
     <div className="flex gap-3">
-      <input
-        value={bpm}
-        onChange={(val) => {
-          changeBPM(Number(val.target.value));
-        }}
-        className="w-9 text-xl text-right bg-inherit outline-none font-bold drop-shadow-lg"
-        style={{caretColor: "transparent"}}
-        type="number"
-        min="60"
-        max="260"
-      />
-      <span className="self-end pb-[3px] text-sm text-light-gray font-semibold">BPM</span>
+      <div className="flex">
+        <input
+          value={bpm}
+          onChange={(val) => {
+            changeBPM(Number(val.target.value));
+          }}
+          className="w-24 text-xl text-center accent-white outline-none font-bold drop-shadow-lg"
+          type="range"
+          step="10"
+          min="60"
+          max="240"
+        />
+      </div>
+      <div className="flex gap-3">
+        <span className="w-8 text-end self-center text-md text-white font-bold">{bpm}</span>
+        <span className="self-center text-sm text-light-gray font-semibold">BPM</span>
+      </div>
     </div>
   );
 }
