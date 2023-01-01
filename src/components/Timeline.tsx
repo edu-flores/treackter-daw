2099beabb458b5715bd6dffea5d8f03b3d7b12a6
Eduardo Flores <eduardo.floresramones@gmail.com>
2022-12-31 22:00:41 -0600

    Hotfix

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -43,7 +43,7 @@ const Timeline = ({ bpm, volume }: Props) => {
       prevPad.active = false;
       const currPad = track.pads[column];
       currPad.active = true;
-      if (currPad.armed && !track.state.muted && (!track.state.ignored || track.state.solo)) {  // Play sound
+      if (currPad.armed  && (track.state.solo || (!track.state.ignored && !track.state.muted))) {  // Play sound
         const soundboardPad = document.getElementById(`${track.name} ${currPad.kit}`);
         soundboardPad?.dispatchEvent(new Event('mousedown'));
         soundboardPad?.classList.add('scale-95');
@@ -55,8 +55,6 @@ const Timeline = ({ bpm, volume }: Props) => {
     setTracks(tracksCopy);
     await new Promise(r => setTimeout(r, 60_000 / bpm));
   }
-
-  console.log(isPlaying);
 
   // Media Buttons SVG and functions
   const playPath = <path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z" />;
